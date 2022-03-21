506a2f1c77395dc3b2d7f4011bc23027cfb0b77c
user <user@DESKTOP-6JBEF5K>
2022-03-21 10:22:26 +0800

    HttpMessageInvoker

--- 20220321/Program.cs
+++ 20220321/Program.cs
@@ -10,7 +10,7 @@ namespace _20220321
     {
         static void Main(string[] args)
         {
-            Console.Write("hello");
+            Console.Write("HttpMessageInvoker");
         }
     }
 }
